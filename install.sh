047ee0fc45c8336e6ce7c1bd2ed0554d346839e2
yoliveros <28690224+yoliveros@users.noreply.github.com>
2025-10-02 13:31:26 -0400

    themenizer for linux-wallpaper done

--- install.sh
+++ install.sh
@@ -1,40 +1,62 @@
 #!/usr/bin/env bash
 
-user=$(logname)
+[ "$(id -u)" != 0 ] && exec sudo -- "$0" "$@"
 
+export USER=$(logname)
+
+# Paru/Pacman dependencies
 dep=(
-  "atuin", 
-  "bat", 
-  "bear", 
-  "btop", 
-  "fastfetch", 
-  "eza", 
-  "fzf", 
-  "wget", 
-  "gdu", 
-  "go", 
-  "ghostty", 
-  "neovim", 
-  "nvm", 
-  "meson", 
-  "pnpm", 
-  "ripgrep",
-  "starship", 
-  "stow", 
-  "tldr", 
-  "tmux", 
-  "unzip", 
-  "zsh",
-  "hypridle",
-  "hyprlock",
-  "hyprpolkintagent", # To do
-  "hyprsysteminfo",
-  "hyprcursor",
-  "hyprsunset",
+  "atuin"
+  "bat" 
+  "bear" 
+  "btop" 
+  "fastfetch"
+  "eza" 
+  "fzf" 
+  "wget" 
+  "gdu" 
+  "go" 
+  "ghostty" 
+  "neovim" 
+  "nvm" 
+  "meson" 
+  "pnpm" 
+  "ripgrep"
+  "starship" 
+  "stow" 
+  "tldr" 
+  "tmux" 
+  "unzip" 
+  "zsh"
+  "hypridle"
+  "hyprlock"
+  "hyprpolkintagent" # To do
+  "hyprsysteminfo"
+  "hyprcursor"
+  "hyprsunset"
+  "linux-wallpaperengine-git"
+  "python-pywal16"
 )
 
-cp ./systemd/themenizer.service.template ./systemd/themenizer.service
-echo ExecStart=/bin/bash /home/$user/.local/bin/themenizer >> ./systemd/themenizer.service
+services=("themenizer.service")
 
-ln -sf /home/$user/.dotfiles/systemd/themenizer.service /etc/systemd/system/themenizer.service
-ln -sf /home/$user/.dotfiles/systemd/themenizer.timer /etc/systemd/system/themenizer.timer
+for service in "${services[@]}"; do
+  cp ./systemd/"${service}".template ./systemd/"${service}"
+  echo ExecStart=/home/"${USER}"/.local/bin/themenizer >> ./systemd/"${service}"
+done
+
+services+=("themenizer.timer")
+
+for file in "${services[@]}"; do
+    ln -sf /home/"${USER}"/.dotfiles/systemd/"${file}" /etc/systemd/system/"${file}"
+done
+
+systemctl daemon-reload
+
+must_init_services=("themenizer.timer")
+
+for service in "${must_init_services[@]}"; do
+    systemctl enable "${service}"
+    systemctl start "${service}"
+done
+
