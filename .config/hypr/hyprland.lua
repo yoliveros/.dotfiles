378fd7ff8334a892469cd48081ea1aa8163abf7c
Yoelvis Oliveros <yoelvisoliveros@gmail.com>
2026-05-11 21:28:54 -0400

    unreal engine removed rule

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -71,6 +71,13 @@ end)
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("SDL_VIDEODRIVER", "wayland")
+hl.env("QT_SCALE_FACTOR_ROUNDING_POLICY", "RoundPreferFloor")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 
 
 -----------------------
@@ -399,15 +406,15 @@ hl.window_rule({
 -- overlayLayerRule:set_enabled(false)
 
 -- Unreal Engine
-hl.window_rule({
-  name = "unreal-editor",
-  match = {
-    class = "^UnrealEditor$",
-    title = "^\\w*$",
-  },
-  no_focus = true,
-  no_anim = true
-})
+-- hl.window_rule({
+--   name = "unreal-editor",
+--   match = {
+--     class = "^UnrealEditor$",
+--     title = "^\\w*$",
+--   },
+--   no_focus = true,
+--   no_anim = true
+-- })
 
 -- Hyprland-run windowrule
 hl.window_rule({
